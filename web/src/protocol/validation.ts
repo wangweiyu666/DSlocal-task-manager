b002ab48d6496420a78f09175771ceabeb4efd55
rochelimit <wangweiyu666gg@gmail.com>
2026-09-06 22:10:16 +0800

    fix: use standalone protocol validator in web
    Signed-off-by: rochelimit <wangweiyu666gg@gmail.com>

--- web/src/protocol/validation.ts
+++ web/src/protocol/validation.ts
@@ -1,8 +1,6 @@
-import Ajv2020 from "ajv/dist/2020";
-import schema from "../../../docs/dst1-schema.json";
 import { validateDst1Batch as validateShared } from "../../../shared/protocol/validation";
+import { validate as validateSchema } from "../../../shared/protocol/schema-validator";
 import type { Dst1Batch } from "./types";
-const validateSchema = new Ajv2020({ allErrors: true, strict: false, validateFormats: false }).compile(schema);
 export function validateDst1Batch(value: unknown): asserts value is Dst1Batch {
   validateShared(value, validateSchema);
 }
